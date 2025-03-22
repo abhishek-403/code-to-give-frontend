18f764b157196f83f682e964bf96eedf82c7e357
Prajwal Tiwari <fnafchica2004@gmail.com>
2025-03-23 02:36:28 +0530

    Added a basic admin dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
@@ -7,8 +6,11 @@ import { FontSizeContext } from '@/contexts/FontSizeContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/auth/LoginPage';
 import SignupPage from './pages/auth/SignupPage';
+
 import EventRegistrationVolunteerPage from './pages/EventRegistrationVolunteerPage';
 import UserProfilePage from './pages/UserProfilePage';
+
+import AdminDashboardPage from './pages/admin/AdminDashboardPage';
 
 const App = () => {
     const [fontSize, setFontSize] = useState(16);
@@ -21,8 +23,11 @@ const App = () => {
                         <Route path="/" element={<Layout><HomePage /></Layout>} />
                         <Route path="/login" element={<Layout><LoginPage /></Layout>} />
                         <Route path="/signup" element={<Layout><SignupPage /></Layout>} />
+
                         <Route path="/volunteer/register/:eventId" element={<Layout><EventRegistrationVolunteerPage /></Layout>} />
                         <Route path="/profile" element={<Layout><UserProfilePage /></Layout>} />
+
+                        <Route path="/admin" element={<Layout><AdminDashboardPage /></Layout>} />
                     </Routes>
                 </Router>
             </FontSizeContext.Provider>
