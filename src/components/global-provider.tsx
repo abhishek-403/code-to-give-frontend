c4b85643acd00636a84add739368a1bf11ce097d
Prajwal Tiwari <fnafchica2004@gmail.com>
2025-03-22 14:36:46 +0530

    removed router from here added it to App.tsx

--- src/components/global-provider.tsx
+++ src/components/global-provider.tsx
@@ -2,8 +2,8 @@ type Props = { children: React.ReactNode };
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect } from "react";
 import toast, { Toaster, useToasterStore } from "react-hot-toast";
-import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "./theme-provider";
+
 export function ToastProvider() {
   const { toasts } = useToasterStore();
   const limit = 3;
@@ -32,13 +32,17 @@ export const queryClient = new QueryClient();
 
 export default function GlobalProvider({ children }: Props) {
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider  defaultTheme="light" storageKey="vite-ui-theme">
-          <ToastProvider />
-          {children}
-        </ThemeProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <ToastProvider />
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
+
+// a good commit message for changes made to this file are:
+// initially google console was showing error for router inside router
+// so i removed the router from here and added it to the App.tsx file
+
+// commit msg: removed router from here and added it to App.tsx file
